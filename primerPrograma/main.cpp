69acb4b931639e7fd152edc564a622d9c0397087
StylezZz <a20202692@pucp.edu.pe>
2023-03-21 09:45:47 -0500

    primer commit

---
+++ primerPrograma/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: chris
+ *
+ * Created on 21 de marzo de 2023, 9:18
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int x;
+    cout<<"Hola Mundo";
+    
+    return 0;
+}
+
